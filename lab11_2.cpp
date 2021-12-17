c3bcd6c81815392a532ef00ac9909500fdd91ad6
naruemonlarpuangkom <narumon.02545@gmail.com>
2021-12-17 15:58:38 +0700

    lab11_2Finnished

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,14 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string text;
+	cout << "-------------------- BOOM ---------------------" << '\n';
+	while (getline(source,text))
+	{
+		cout << text << '\n';
+	}
+	cout << "-------------------- HA!! ---------------------" << '\n';
+
 
     source.close();
     dest.close();
